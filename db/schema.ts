0897bb52cb02baa05441d8c3ebafe8837bfb1267
Andres Aguirre <aguirregzz97@gmail.com>
2023-12-16 20:36:42 -0600

    add categories migration

--- db/schema.ts
+++ db/schema.ts
@@ -6,24 +6,29 @@ import {
   timestamp,
 } from "drizzle-orm/mysql-core";
 
-export const tournaments = mysqlTable("tournaments", {
+// export const tournaments = mysqlTable("tournaments", {
+//   id: serial("id").autoincrement(),
+//   owner_id: varchar("owner_id", { length: 256 }),
+//   name: varchar("name", { length: 256 }),
+//   country: varchar("country", { length: 256 }),
+//   city: varchar("city", { length: 256 }),
+//   address: varchar("city", { length: 256 }),
+//   banner_url: varchar("banner", { length: 2084 }),
+//   categories: mysqlEnum("categories", [
+//     "Primera",
+//     "Segunda",
+//     "Tercera",
+//     "Cuarta",
+//     "Quinta",
+//     "Sexta",
+//     "Septima",
+//     "Octava",
+//   ]),
+//   created_at: timestamp("created_at").defaultNow(),
+//   updated_at: timestamp("updated_at").defaultNow(),
+// });
+
+export const categories = mysqlTable("categories", {
   id: serial("id").autoincrement(),
-  owner_id: varchar("owner_id", { length: 256 }),
-  name: varchar("name", { length: 256 }),
-  country: varchar("country", { length: 256 }),
-  city: varchar("city", { length: 256 }),
-  address: varchar("city", { length: 256 }),
-  banner_url: varchar("banner", { length: 2084 }),
-  categories: mysqlEnum("categories", [
-    "Primera",
-    "Segunda",
-    "Tercera",
-    "Cuarta",
-    "Quinta",
-    "Sexta",
-    "Septima",
-    "Octava",
-  ]),
-  created_at: timestamp("created_at").defaultNow(),
-  updated_at: timestamp("updated_at").defaultNow(),
+  category_name: varchar("category_name", { length: 256 }),
 });
---
+++ drizzle/0000_chubby_wraith.sql
@@ -0,0 +1,4 @@
+CREATE TABLE `categories` (
+	`id` serial AUTO_INCREMENT,
+	`category_name` varchar(256)
+);
